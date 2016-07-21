c1f14719f46eacf813e4ba6a17940d3a7d7e7dd9
adrianachitzu <adriana_chitzu@yahoo.com>
2016-07-21 18:22:44 +0300

    teme lab18

---
+++ Adriana/teme/js/script.js
@@ -0,0 +1,18 @@
+//array de numere minim
+
+function minnumere (val){
+	var minarr =[];
+	for (i = 0; i < val; i++){
+		minarr[i]= minarr.min();
+
+	minnumere (val);
+
+
+//array de numere maxim
+
+function maxnumere (val){
+	var maxarr =[];
+	for (i = 0; i < val; i++){
+		minarr[i]= maxarr.max();
+
+	maxnumere (val);
